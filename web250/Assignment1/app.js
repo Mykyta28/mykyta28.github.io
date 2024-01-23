ea09147d9108e37f0523405103c1daa4ced13686
Nikita Semenii <115111003+Mykyta28@users.noreply.github.com>
2024-01-22 22:09:16 -0600

    Update app.js

--- web250/Assignment1/app.js
+++ web250/Assignment1/app.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const app = express();
 
+app.get('/', (request, response) => {
+    response.send('Hello Mykyta Semenii!');
+});
+
+app.listen(3000, () => console.log('server started'));
